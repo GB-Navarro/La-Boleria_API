6623aab6a3d7e92a99fab7dc3a2ecbf5485e8507
GB-Navarro <gbnavarro@usp.br>
2022-08-17 13:23:14 -0300

    fix: fixed a validation issue with phone atributte

--- middlewares/clientsMiddlewares.js
+++ middlewares/clientsMiddlewares.js
@@ -16,7 +16,7 @@ export function validateInsertClientSchema(req, res, next){
         const phoneEmptyError = '"phone" is not allowed to be empty';
         const phoneMinLengthError = '"phone" length must be at least 10 characters long';
         const phoneMaxLengthError = '"phone" length must be less than or equal to 11 characters long';
-        const phoneRegexError = `"phone" with value "${phone}" fails to match the required pattern: /[^0-9]{10,11}$/`
+        const phoneRegexError = `"phone" with value "${phone}" fails to match the required pattern: /^[0-9]{10,11}$/`
         
         console.log(validateErrorMessage);
         if(validateErrorMessage === nameRequiredError || validateErrorMessage === nameEmptyError){
