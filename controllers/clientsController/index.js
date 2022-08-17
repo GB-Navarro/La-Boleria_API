f13d63a080f6c55c9848a524236626ef610190b7
GB-Navarro <gbnavarro@usp.br>
2022-08-16 23:08:34 -0300

    feat: a function to insert a new client in the database has been created

--- controllers/clientsController/index.js
+++ controllers/clientsController/index.js
@@ -1,5 +1,18 @@
+import clientsRepository from "./../../repositories/clientsRepository.js";
+
 async function insertClient(req,res){
-   res.send("Hello World!");
+    const {name, adress, phone} = req.body;
+    try{
+        const result = await clientsRepository.insertClient(name, adress, phone);
+        if(result.rowCount === 1){
+            res.sendStatus(201);
+        }else{
+            res.sendStatus(500);
+        }
+    }catch(error){
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
 
 const clientsController = {
