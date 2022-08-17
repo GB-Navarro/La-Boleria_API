e29f562e53ea5f116d405e216a6a46fd41f36666
GB-Navarro <gbnavarro@usp.br>
2022-08-16 22:44:50 -0300

    feat: the insertClientSchema has been created

---
+++ schemas/clientsSchemas.js
@@ -0,0 +1,13 @@
+import Joi from "joi";
+
+const insertClientSchema = Joi.object({
+    name: Joi.string().min(1).required(),
+    adress: Joi.string().min(1).required(),
+    phone: Joi.string().min(10).max(11).required()
+})
+
+const clientsSchemas = {
+    insertClientSchema
+}
+
+export default clientsSchemas;
